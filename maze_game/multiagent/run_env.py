a6f8ac2f1904606f476dcd0f482e44643214b504
Dmitry Kozerovsky <dima29koz@yandex.ru>
2023-04-28 16:39:43 +0300

    refactored run_env script

--- maze_game/multiagent/run_env.py
+++ maze_game/multiagent/run_env.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 
 import ray
 from ray.rllib.algorithms.ppo import PPO, PPOConfig
@@ -13,7 +14,6 @@ from maze_game.multiagent.maze_multi_agent_env import MAMazeGameEnv, create_env
 from maze_game.multiagent.actions import action_to_action_space
 from maze_game.multiagent.models.action_masking import TorchActionMaskModel
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", help="checkpoint path")
 
@@ -22,85 +22,142 @@ def new_policy_mapping_fn(agent_id, episode, worker, **kwargs):
     return 'main'
 
 
-def manual_policy(env, agent=None, observation=None):
-    if env.unwrapped.gui is None:
-        env.render()
-    act = None
-    act_pl_abilities = env.unwrapped.game.get_allowed_abilities(env.unwrapped.game.get_current_player())
-    while not act:
-        act, state = env.unwrapped.gui.get_action(act_pl_abilities)
-    return action_to_action_space(act)
+class RLGame:
+    def __init__(self):
+        env_name = "maze_game_v2"
+        self.num_players = 2
 
+        register_env(env_name, lambda config: PettingZooEnv(create_env(num_players=self.num_players)))
+        ModelCatalog.register_custom_model("pa_model", TorchActionMaskModel)
 
-def random_policy(env, agent, observation):
-    return env.action_space(agent).sample(observation["action_mask"])
+        args = parser.parse_args()
+        checkpoint_path = os.path.expanduser(args.path)
 
-
-def ppo_policy(algo, agent, observation):
-    policy_id = policy_mapping_fn(agent)
-    # policy_id = new_policy_mapping_fn(agent, None, None)
-    return algo.compute_single_action(observation, policy_id=policy_id)
-
-
-def run(num_resets=1):
-    env_name = "maze_game_v2"
-    num_players = 2
-
-    env = create_env(render_mode="human", num_players=num_players)
-    env.metadata['render_fps'] = 10
-
-    register_env(env_name, lambda config: PettingZooEnv(create_env(num_players=num_players)))
-    ModelCatalog.register_custom_model("pa_model", TorchActionMaskModel)
-
-    args = parser.parse_args()
-    checkpoint_path = os.path.expanduser(args.path)
-
-    ray.init(local_mode=True, num_cpus=0)
-    config = (
-        PPOConfig()
-        .environment(env=env_name)
-        .training(
-            model={'custom_model': 'pa_model'}
+        ray.init(local_mode=True, num_cpus=0)
+        config = (
+            PPOConfig()
+            .environment(env=env_name)
+            .rollouts(
+                num_rollout_workers=0,
+            )
+            .training(
+                model={'custom_model': 'pa_model'}
+            )
+            .multi_agent(
+                policies=policies,
+                policy_mapping_fn=policy_mapping_fn,
+                # policies={
+                #     'main': PolicySpec(
+                #         policy_class=None,
+                #         observation_space=obs_space,
+                #         action_space=act_space,
+                #     ),
+                # },
+                # policy_mapping_fn=new_policy_mapping_fn,
+                policies_to_train=[],
+            )
+            .debugging(log_level="ERROR")
+            .framework(framework="torch")
+            .resources(num_gpus=1)
         )
-        .multi_agent(
-            policies=policies,
-            policy_mapping_fn=policy_mapping_fn,
-            # policies={
-            #     'main': PolicySpec(
-            #         policy_class=None,
-            #         observation_space=obs_space,
-            #         action_space=act_space,
-            #     ),
-            # },
-            # policy_mapping_fn=new_policy_mapping_fn,
-            policies_to_train=[],
-        )
-        .debugging(log_level="ERROR")
-        .framework(framework="torch")
-        .resources(num_gpus=1)
-    )
 
-    ppo_agent = PPO(config)
-    ppo_agent.restore(checkpoint_path)
-    for i in range(num_resets):
-        env.reset()
-        print('reset number', i)
-        for agent in env.agent_iter():
-            observation, reward, termination, truncation, info = env.last()
-            print(info, f'reward={reward}')
-            if termination or truncation:
-                action = None
-            else:
-                # this is where you would insert your policy
-                # action = random_policy(env, agent, observation)
-                # action = manual_policy(env, agent, observation)
-                action = ppo_policy(ppo_agent, agent, observation)
+        self.ppo_agent = PPO(config)
+        self.ppo_agent.restore(checkpoint_path)
+        self.env = None
 
-            env.step(action)
+    def manual_policy(self, agent=None, observation=None):
+        if self.env.unwrapped.gui is None:
+            self.env.render()
+        act = None
+        act_pl_abilities = self.env.unwrapped.game.get_allowed_abilities(self.env.unwrapped.game.get_current_player())
+        while not act:
+            act, state = self.env.unwrapped.gui.get_action(act_pl_abilities)
+        return action_to_action_space(act)
 
-            env.render()
-    env.close()
+    def random_policy(self, agent, observation):
+        return self.env.action_space(agent).sample(observation["action_mask"])
+
+    def ppo_policy(self, agent, observation):
+        policy_id = policy_mapping_fn(agent)
+        # policy_id = new_policy_mapping_fn(agent, None, None)
+        return self.ppo_agent.compute_single_action(observation, policy_id=policy_id)
+
+    def run_performance_test(self, num_resets=100):
+        self.env = create_env(render_mode=None, num_players=self.num_players)
+        self.env.metadata['render_fps'] = 10
+
+        all_times = []
+        all_steps = []
+        all_steps_tr = []
+        sh = 0
+        sh_s = 0
+        for i in range(num_resets):
+            self.env.reset()
+            steps = 0
+            times = []
+            tr_step = 0
+            print('reset number', i)
+            for agent in self.env.agent_iter():
+                time_start = time.time()
+                observation, reward, termination, truncation, info = self.env.last()
+                # print(info, f'reward={reward}')
+                if termination or truncation:
+                    action = None
+                else:
+                    # this is where you would insert your policy
+                    # action = self.random_policy(agent, observation)
+                    # action = self.manual_policy(agent, observation)
+                    action = self.ppo_policy(agent, observation)
+
+                self.env.step(action)
+                time_end = time.time() - time_start
+                times.append(time_end)
+                if tr_step == 0 and info.get('turn_info').get('action') == 'swap_treasure' and info.get(
+                        'resp') == 'подобрал клад':
+                    tr_step = steps
+
+                if info.get('turn_info').get('action') == 'shoot_bow':
+                    sh += 1
+                    if info.get('resp').find('ранены') != -1:
+                        sh_s += 1
+                steps += 1
+            all_steps.append(steps)
+            all_steps_tr.append(tr_step)
+            all_times.append(times)
+        self.env.close()
+        return {
+            'steps': all_steps,
+            'tr_steps': all_steps_tr,
+            'times': all_times,
+            'shooting res': (sh, sh_s)
+        }
+
+    def run(self, num_resets=1):
+        self.env = create_env(render_mode='human', num_players=self.num_players)
+        self.env.metadata['render_fps'] = 10
+
+        for i in range(num_resets):
+            self.env.reset()
+
+            print('reset number', i)
+            for agent in self.env.agent_iter():
+                observation, reward, termination, truncation, info = self.env.last()
+                print(info, f'reward={reward}')
+                if termination or truncation:
+                    action = None
+                else:
+                    # this is where you would insert your policy
+                    # action = self.random_policy(agent, observation)
+                    # action = self.manual_policy(agent, observation)
+                    action = self.ppo_policy(agent, observation)
+
+                self.env.step(action)
+                self.env.render()
+        self.env.close()
 
 
 if __name__ == "__main__":
-    run(100)
+    game = RLGame()
+    game.run(100)
+    # res = game.run_performance_test(100)
+    # print(res)
